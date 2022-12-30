1f0bc3652c95fc81675c6a9dbc4984d1cb7d912d
feruzu <fernanda23demarzo@gmail.com>
2022-12-30 13:13:19 -0300

    Commit

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-let añadir = document.querySelector('.añadir-nota');
-let aceptar = document.querySelector('#aceptar');
-let asunto = document.querySelector('#asunto-nota');
-let nota = document.querySelector('#texto-nota');
-let modal = document.querySelector('.modal');
-let texto = document.querySelector('#texto-nota');
-let textoNota = document.querySelector('texto');
-let vacio = document.querySelector('.notas-vacio');
-let notaNueva = document.querySelector('.nota-nueva');
-
-aceptar.addEventListener('click', añadirNota);
+let añadir = document.querySelector(".añadir-nota");
+let aceptar = document.querySelector("#aceptar");
+let asunto = document.querySelector("#asunto-nota");
+let nota = document.querySelector("#texto-nota");
+let modal = document.querySelector(".modal");
+let texto = document.querySelector("#texto-nota");
+let textoNota = document.querySelector("texto");
+let vacio = document.querySelector(".notas-vacio");
+let notaNueva = document.querySelector(".nota-nueva");
 let notas = [];
+
+aceptar.addEventListener("click", añadirNota);
 
 function añadirNota() {
   // Añade nota
@@ -25,84 +25,70 @@ function añadirNota() {
         </div>`),
       document.querySelector(".note").appendChild(newElement);
 
-      vacio.style.display = "none";
+    vacio.style.display = "none";
   }
 
+  // Guardar en el LocalStorage
+  guardarLocalStorage();
+  function guardarLocalStorage() {
+    const nota = {
+      asunto: asunto.value,
+      texto: texto.value,
+    };
 
-// Guardar en el LocalStorage
-guardarLocalStorage()
-function guardarLocalStorage(){
-  const nota = {
-    asunto: asunto.value,
-    texto: texto.value
-  };
-
-  if(localStorage.getItem('notas') === null){
-    notas.push(nota);
-    localStorage.setItem("notas", JSON.stringify(notas));
-  }else{
-    notas = JSON.parse(localStorage.getItem('notas'));
-    notas.push(nota);
-    localStorage.setItem("notas", JSON.stringify(notas));
-} 
-
+    if (localStorage.getItem("notas") === null) {
+      notas.push(nota);
+      localStorage.setItem("notas", JSON.stringify(notas));
+    } else {
+      notas = JSON.parse(localStorage.getItem("notas"));
+      notas.push(nota);
+      localStorage.setItem("notas", JSON.stringify(notas));
+    }
+  }
 }
-
-}
-
-
 
 // Borrar notas
-borrarNotas()
+borrarNotas();
 function borrarNotas(asunto) {
-  if(asunto != null ){
-    notas = notas.filter(nota => nota.asunto != asunto);
+  if (asunto != null) {
+    notas = notas.filter((nota) => nota.asunto !== asunto);
     localStorage.setItem("notas", JSON.stringify(notas));
   }
   recuperar();
-
 }
 
-
 // Recuperar localStorage Notas
-recuperar()
-function recuperar(){
-  if(localStorage.getItem('notas') !== null){
+function recuperar() {
+  if (localStorage.getItem("notas") !== null) {
     vacio.style.display = "none";
   }
-  
 
   let array = [];
-  array = (JSON.parse(localStorage.getItem('notas')))
+  array = JSON.parse(localStorage.getItem("notas"));
 
   reset();
-   array.map((element,index) => {
-     const newElement = document.createElement("div");
-     newElement.classList.add("nota-nueva");
-     document.querySelector(".note").appendChild(newElement);
 
-     (newElement.innerHTML = `<div class="nota">
+  array &&
+    array.map((element, index) => {
+      const newElement = document.createElement("div");
+      newElement.classList.add("nota-nueva");
+      document.querySelector(".note").appendChild(newElement);
+
+      (newElement.innerHTML = `<div class="nota">
         <img id="borrar" src="img/borrar.svg" onclick="borrarNotas('${element.asunto}')">
          <h2 id="asunto">${element.asunto}</h2>
          <p class="texto">${element.texto}</p>
          </div>`),
-       document.querySelector(".note").appendChild(newElement);
-       
-   })
+        document.querySelector(".note").appendChild(newElement);
+
+      notas = JSON.parse(localStorage.getItem("notas"));
+    });
 }
-
-
-
 
 // Limpiar recuperar
-function reset(){
+function reset() {
   const elements = document.getElementsByClassName("nota-nueva");
-  while(elements.length > 0){
-      elements[0].parentNode.removeChild(elements[0]);
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
   }
 }
-
-
-
-
-
